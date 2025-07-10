d16c2f52c30e229ae046f2dab50034cba1fc6b31
kewayne <34725741+kewayne@users.noreply.github.com>
2025-07-10 14:37:22 -0500

    Create version.php

---
+++ version.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Version details for the Course SMS local plugin.
+ *
+ * @package   local_coursessms
+ * @copyright 2025 Me
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_coursessms';
+$plugin->version = 2025071000; // YYYYMMDDXX (Year, Month, Day, increment).
+$plugin->requires = 2023100900; // Requires Moodle 4.3.
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '0.1';

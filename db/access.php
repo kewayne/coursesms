c81c1ee9d70c13e1a0bd78f03c3ea21744c8c5e3
kewayne <34725741+kewayne@users.noreply.github.com>
2025-07-10 14:37:46 -0500

    Create access.php

---
+++ db/access.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Course SMS plugin capabilities.
+ *
+ * @package   local_coursessms
+ * @copyright 2025 Me
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+    'local/coursessms:sendsms' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'coursecreator' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+    ],
+    'local/coursessms:viewlog' => [
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'coursecreator' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+    ],
+];

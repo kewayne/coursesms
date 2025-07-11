047586e6cfb1f3d7bd44fac1c955be67fe8e300d
kewayne <34725741+kewayne@users.noreply.github.com>
2025-07-11 05:46:31 -0500

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'local_coursessms';
-$plugin->version = 2025071000;
+$plugin->version = 2025071100;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
